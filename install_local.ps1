95990f1cd8a0a4a635eba519688062fea7495098
Igor Spichkin <igorsa.ux@outlook.com>
2021-03-29 14:59:44 +0300

    Core 1.0.3

--- install_local.ps1
+++ install_local.ps1
@@ -1,3 +1,3 @@
-dotnet pack ./src/ChaoticOnyx.Tools.Core/
+dotnet pack -c Release ./src/ChaoticOnyx.Tools.Core/
 dotnet tool uninstall --global ChaoticOnyx.Tools
 dotnet tool install --global --add-source ./artifacts/bin/ChaoticOnyx.Tools.Core/ ChaoticOnyx.Tools
